b5c6d4fdd3b281c4337b37f3013c8f4c9f6d2c59
mhutchens <mhutchens21144@gmail.com>
2024-06-19 11:43:14 -0700

    Add clang-tidy checks and fix warnings
    - Adds the .clang-tidy config file and fixes generated warnings.

--- src/pool_party.hpp
+++ src/pool_party.hpp
@@ -24,6 +24,12 @@ class movable_callable {
     struct impl_base {
         virtual void call() = 0;
         virtual ~impl_base() = default;
+
+        impl_base() = default;
+        impl_base(const impl_base&) = delete;
+        impl_base(impl_base&&) = default;
+        impl_base& operator=(const impl_base&) = delete;
+        impl_base& operator=(impl_base&&) = default;
     };
 
     std::unique_ptr<impl_base> impl_;
@@ -43,9 +49,9 @@ class movable_callable {
     movable_callable() = default;
     movable_callable(const movable_callable&) = delete;
     movable_callable(movable_callable&) = delete;
-    movable_callable(movable_callable&& other)
+    movable_callable(movable_callable&& other) noexcept
         : impl_(std::move(other.impl_)) {}
-    movable_callable& operator=(movable_callable&& other) {
+    movable_callable& operator=(movable_callable&& other) noexcept {
         impl_ = std::move(other.impl_);
         return *this;
     }
@@ -72,12 +78,14 @@ class thread_pool {
     void start() {
         // Double check lock to avoid needing to lock if the threads are already
         // running
-        if (is_running_)
+        if (is_running_) {
             return;
-        std::unique_lock lk(thread_pool_lock_);
+        }
 
-        if (is_running_)
+        std::unique_lock pool_lock(thread_pool_lock_);
+        if (is_running_) {
             return;
+        }
 
         threads_ = std::vector<std::jthread>(num_threads_);
         for (size_t i = 0; i < num_threads_; i++) {
@@ -89,16 +97,18 @@ class thread_pool {
     void stop() {
         // Double check lock to avoid needing to lock if the threads are already
         // running
-        if (!is_running_)
+        if (!is_running_) {
             return;
-        std::unique_lock lk(thread_pool_lock_);
+        }
+        std::unique_lock pool_lock(thread_pool_lock_);
 
-        if (!is_running_)
+        if (!is_running_) {
             return;
+        }
 
-        std::unique_lock q_lk(queue_lock_);
+        std::unique_lock q_lock(queue_lock_);
         shutting_down_ = true;
-        q_lk.unlock();
+        q_lock.unlock();
 
         queue_cv_.notify_all();
         for (auto& thread : threads_) {
@@ -107,19 +117,18 @@ class thread_pool {
         threads_.clear();
         shutting_down_ = false;
         is_running_ = false;
-        return;
     }
 
-    size_t num_threads() const { return num_threads_; }
+    [[nodiscard]] size_t num_threads() const { return num_threads_; }
 
-    bool is_running() const { return is_running_; }
+    [[nodiscard]] bool is_running() const { return is_running_; }
 
     template <typename Callable, typename... Args,
               typename ReturnType = std::invoke_result_t<Callable, Args...>>
     [[nodiscard]] auto submit(Callable&& task, Args&&... args) {
         std::promise<ReturnType> task_promise;
         std::future<ReturnType> task_future = task_promise.get_future();
-        std::unique_lock lk(queue_lock_);
+        std::unique_lock q_lock(queue_lock_);
         if constexpr (std::is_same_v<ReturnType, void>) {
 
             task_queue_.emplace(
@@ -139,7 +148,7 @@ class thread_pool {
                     std::forward<Callable>(task), std::forward<Args>(args)...));
             });
         }
-        lk.unlock();
+        q_lock.unlock();
         queue_cv_.notify_one();
         return task_future;
     }
@@ -147,13 +156,14 @@ class thread_pool {
   private:
     void worker_task(size_t thread_num) {
         while (true) {
-            std::unique_lock lk(queue_lock_);
-            queue_cv_.wait(lk, [this] {
-                return shutting_down_ || task_queue_.size() > 0;
+            std::unique_lock q_lock(queue_lock_);
+            queue_cv_.wait(q_lock, [this] {
+                return shutting_down_ || !task_queue_.empty();
             });
 
-            if (shutting_down_)
+            if (shutting_down_) {
                 return;
+            }
 
             auto task = std::move(task_queue_.front());
             task_queue_.pop();
